f2c6030db11169583dd2a9be1643bfbe3a856a79
Semih Katmerlikaya <smhktmrlky@gmail.com>
2022-05-21 16:46:02 +0300

    card component background added

--- src/components/CardComponentForTen.js
+++ src/components/CardComponentForTen.js
@@ -5,17 +5,27 @@ import { monthNames, dayNames } from "../constants/MonthNames";
 const CardComponentForTen = ({ day, modalControl }) => {
   const now = new Date();
   const date = new Date(day);
+  const dateRight =
+    date.getDate() +
+    " " +
+    monthNames[date.getMonth()] +
+    " " +
+    date.getFullYear();
+  const input = JSON.parse(localStorage.getItem("dailyActivities"));
+  const daily = input.find((obj) => obj.day === dateRight);
 
   const controlPoint =
     date.getDate() === now.getDate() &&
     date.getMonth() === now.getMonth() &&
     date.getFullYear() === now.getFullYear();
 
+  const controlPoint2 = daily !== undefined && daily.activities.length > 0;
+
   return (
     <div onClick={() => modalControl(date)} style={{ cursor: "pointer" }}>
       <Card
-        bg={controlPoint ? "secondary" : "light"}
-        text={controlPoint ? "light" : "secondary"}
+        bg={controlPoint2 ? "success" : controlPoint ? "secondary" : "light"}
+        text={controlPoint2 ? "light" : controlPoint ? "light" : "secondary"}
       >
         <Card.Body className="m-auto">
           <Card.Title className="text-center">
